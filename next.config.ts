65a82fb07bbb5dcaa0d1072f76c443feb7781134
Shree SitaRam <sagarmanghnani@Shrees-Mac-mini.local>
2026-04-03 17:35:48 +0530

    Fixed image issue

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,14 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'images.unsplash.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'optim.tildacdn.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'static.tildacdn.com',
+      },
     ],
   },
 };
